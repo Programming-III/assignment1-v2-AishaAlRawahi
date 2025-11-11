bd7ddbd55a12cea7cd23a4e2aea3bb37442771ba
AishaAlRawahi <aishaalrawahi06@gmail.com>
2025-11-11 09:50:32 +0200

    Add Course class with basic structure and methods

--- Course.h
+++ Course.h
@@ -6,17 +6,19 @@
 using namespace std;
 //#write your code here
 
-
-
-
-
-
-
-
-
-
-
-
-
+class Course{
+  private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students; //dynamically allocated array
+    int currentStudents;
+  
+  public:
+    void addStudent(const Student& s){}
+    void displayCourseInfo(){}
+    Course(string cc, string cn, int ms){}
+    ~Course(){}
+};
 
 #endif
