320aaf223edadf82c5758dde8b67d7ff7e02a710
AishaAlRawahi <aishaalrawahi06@gmail.com>
2025-11-11 09:51:08 +0200

    Implement Instructor class with constructor and display
    Define Instructor class inheriting from Person.

--- Instructor.h
+++ Instructor.h
@@ -5,15 +5,15 @@
 using namespace std;
 //#write Instructor class here
 
-
-
-
-
-
-
-
-
-
-
-
+class Instructor :: public Person{
+  private:
+    string department;
+    int experienceYears;
+  
+  public:
+    display(){}
+    Instructor(){}
+    Instructor(string n, int i, string d, int e) :: Person(n, i){}
+    ~Instructor(){}
+};
 #endif
